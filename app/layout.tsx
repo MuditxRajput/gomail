00c725e2603472d46a30f6aa15fc041b1b6791dc
Mudit Rajput <muditrajputpersonal@gmail.com>
2025-07-05 17:39:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><meta name="google-site-verification" content="J2LTycitkfG5snmcnyfLLodGAvdItHJFUQjMfm5Cu3Y" /></head>
       <body className={inter.className}>{children}</body>
     </html>
   )
